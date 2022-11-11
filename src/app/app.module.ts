7f073ff597b5981c0815bd831f05e1ca0667e8e9
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-11-11 13:47:53 +0700

    design pattern end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+// import
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],

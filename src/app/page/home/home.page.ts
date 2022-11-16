fc31215a507a660c74fc3d9d93cd74fa4ae4bb44
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-11-16 14:42:43 +0700

    Update Fix slider

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -22,7 +22,15 @@ export class HomePage implements OnInit {
     speed: 500,
   };
   constructor() {}
-  ngOnInit() {
+  /// trong vòng đời hàm ionViewDidEnter sẽ chạy khi router tới trang đó
+  async ionViewDidEnter() {
+    let newIndex = await this.slides.getActiveIndex(); // getActiveIndex trả về số promise index của trang hiện tại trên slider
+    newIndex++;
+    // Workaround to make it work: breaks the animation
+    this.slides.slideTo(newIndex, 0, false);
+  }
+  ngOnInit() {}
+  load() {
     setInterval(() => {
       this.slides.slideNext(1000);
     }, 1000);
